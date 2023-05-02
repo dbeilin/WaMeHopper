080a00efa2af5fff51871608f8a44d7ae7f991a7
Daniel Beilin <beylin.daniel@gmail.com>
2023-05-02 12:49:25 +0300

    new features, bug fixes

--- background.js
+++ background.js
@@ -1,43 +1,61 @@
+function isValidPhoneNumber(phoneNumber) {
+  const regex = /^\+?\d{10,15}$/;
+  return regex.test(phoneNumber);
+}
+
 function openWhatsApp(info) {
-    const phoneNumber = info.selectionText.replace(/[-()\s]/g, '');
-  
-    browser.storage.local.get(['countryCode', 'delay']).then((data) => {
-      const countryCode = data.countryCode || '972';
-      const delay = parseInt(data.delay || '0') * 1000;
-      const whatsappUrl = `https://wa.me/${countryCode}${phoneNumber}`;
-  
-      // Create a new tab for the WhatsApp URL
-      return browser.tabs.create({ url: whatsappUrl, active: false }).then((tab) => {
-        return { tab, delay };
-      });
-    }).then(({ tab, delay }) => {
-      // Wait for the tab to finish loading
-      return new Promise((resolve) => {
-        browser.tabs.onUpdated.addListener(function listener(tabId, changeInfo) {
-          if (tabId === tab.id && changeInfo.status === 'complete') {
-            browser.tabs.onUpdated.removeListener(listener);
-            resolve({ tab, delay });
-          }
-        });
-      });
-    }).then(({ tab, delay }) => {
-      // Close the wa.me tab after the specified delay
-      setTimeout(() => {
-        browser.tabs.remove(tab.id);
-      }, delay);
-    });
+  const phoneNumber = info.selectionText.replace(/[-()\s]/g, '');
+
+  if (!isValidPhoneNumber(phoneNumber)) {
+    console.log('Invalid phone number:', phoneNumber);
+    return;
   }
-  
-  browser.contextMenus.create({
-    id: 'openWhatsApp',
-    title: 'Open WhatsApp',
-    contexts: ['selection'],
-    documentUrlPatterns: ['*://*/*']
+
+  browser.storage.local.get(['countryCode', 'delay', 'openWith']).then((data) => {
+    const countryCode = data.countryCode || '972';
+    const delay = parseInt(data.delay || '0') * 1000;
+    const openWith = data.openWith || 'whatsappWeb';
+    const whatsappUrl = openWith === 'whatsappWeb' ? `https://web.whatsapp.com/send?phone=${countryCode}${phoneNumber}` : `https://wa.me/${countryCode}${phoneNumber}`;
+
+    // Create a new tab for the WhatsApp URL
+    return browser.tabs.create({ url: whatsappUrl, active: false }).then((tab) => {
+      return { tab, delay };
+    });
+  }).then(({ tab, delay }) => {
+    // Wait for the tab to finish loading
+    return new Promise((resolve) => {
+      browser.tabs.onUpdated.addListener(function listener(tabId, changeInfo) {
+        if (tabId === tab.id && changeInfo.status === 'complete') {
+          browser.tabs.onUpdated.removeListener(listener);
+          resolve({ tab, delay });
+        }
+      });
+    });
+  }).then(({ tab, delay }) => {
+    // Close the wa.me tab after the specified delay
+    setTimeout(() => {
+      browser.tabs.remove(tab.id);
+    }, delay);
   });
-  
-  browser.contextMenus.onClicked.addListener((info, tab) => {
-    if (info.menuItemId === 'openWhatsApp') {
-      openWhatsApp(info);
-    }
-  });
-  
+}
+
+browser.contextMenus.create({
+  id: 'openWhatsApp',
+  title: 'Open WhatsApp',
+  contexts: ['selection'],
+  documentUrlPatterns: ['*://*/*']
+});
+
+browser.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === 'openWhatsApp') {
+    openWhatsApp(info);
+  }
+});
+
+function showWelcomePage(details) {
+  if (details.reason === 'install') {
+    browser.tabs.create({ url: 'welcome.html' });
+  }
+}
+
+browser.runtime.onInstalled.addListener(showWelcomePage);
