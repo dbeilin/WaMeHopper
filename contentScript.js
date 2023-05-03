fd25b174ee41231bc11934e84add1f24f8c1ccc9
Daniel Beilin <beylin.daniel@gmail.com>
2023-05-03 16:55:12 +0300

    templating

---
+++ contentScript.js
@@ -0,0 +1,56 @@
+window.startWhatsAppAutomation = function(phoneNumber, templateText) {
+    function waitForElement(selector, callback) {
+      const element = document.querySelector(selector);
+      if (element) {
+        callback(element);
+      } else {
+        setTimeout(() => waitForElement(selector, callback), 100);
+      }
+    }
+  
+    function typeMessage(inputElement, message) {
+      const event = new InputEvent('input', { bubbles: true });
+      inputElement.innerHTML = message;
+      inputElement.dispatchEvent(event);
+    }
+  
+    function openChatByNumber(phoneNumber) {
+      const contactElement = [...document.querySelectorAll('.chat-title')].find(element => {
+        return element.getAttribute('title') === phoneNumber;
+      });
+  
+      if (contactElement) {
+        contactElement.click();
+        return true;
+      } else {
+        return false;
+      }
+    }
+  
+    function openNewChatAndTypeMessage(phoneNumber, message) {
+      const newChatButton = document.querySelector('button[title="New chat"]');
+  
+      if (newChatButton) {
+        newChatButton.click();
+  
+        waitForElement('input[type="text"]', (inputElement) => {
+          typeMessage(inputElement, phoneNumber);
+          waitForElement(`.chat-title[title="${phoneNumber}"]`, (contactElement) => {
+            contactElement.click();
+            waitForElement('div[contenteditable="true"]', (messageInputElement) => {
+              typeMessage(messageInputElement, message);
+            });
+          });
+        });
+      }
+    }
+  
+    if (!openChatByNumber(phoneNumber)) {
+      openNewChatAndTypeMessage(phoneNumber, templateText);
+    } else {
+      waitForElement('div[contenteditable="true"]', (messageInputElement) => {
+        typeMessage(messageInputElement, templateText);
+      });
+    }
+  };
+  
